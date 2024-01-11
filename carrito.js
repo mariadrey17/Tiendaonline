4025b68cd9061d9e92dba322d2213d0c4a3bb7e4
mariadrey17 <belydrey@yahoo.com.ar>
2024-01-11 19:36:58 -0300

    traspase algunos elementos al proyecto original

---
+++ carrito.js
@@ -0,0 +1,32 @@
+
+
+//añadir producto al carrito primera forma (no funcionó)
+
+//class Carrito {
+    //comprarProducto (e) {
+       // e.preventDefault() ;
+        //if (e.target.classList.contains('agregar-carrito')){
+           // const producto =e.target.parentElement.parentElement;
+           // this .leerDatosProducto(producto);
+           //console.log(producto);
+        //}
+    //}
+    //otro metodo para leer datos
+    //leerDatosProducto (producto){
+       // const infoProducto={
+          //  imagen:producto.document.getElementById ('carrito-image'),
+          //  titulo:producto.document.getElementByClassName('card-title'),
+           // precio:producto.document.getElementByClassName('.card-text'),
+           // id:producto.document.getAttribute('a').getAttribute ('data-id'),
+          // cantidad:1
+        //}
+        //this.insertarCarrito(infoProducto);
+
+        //segunda forma de añadir productos al carrito 
+
+
+  
+         
+   
+
+
